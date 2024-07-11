b643dca652183c1184bc5c4f4eff29cb606f8c72
Hasan Raza <121584126+mbilal-09@users.noreply.github.com>
2024-07-11 14:50:30 +0500

    name edited

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Coffee'),
+        title: const Text('My Coffee App'),
         backgroundColor: Colors.brown[600],
         centerTitle: true,
         titleTextStyle: const TextStyle(
